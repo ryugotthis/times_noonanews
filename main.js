d105aa88aaee67a7fa2729b29f11caa819f20333
Subin <ryusb981211@gmail.com>
2024-07-09 15:26:25 +0900

    third commit

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // const API_KEY = 'a7c947ae7ecb44d6baf7c8b20bcd20e8';
 let news = [];
 const getLatestNews = async () => {
-  const url = new URL(`https://yougotthis-noonanews.netlify.app/top-headlines`);
+  const url = new URL(
+    `https://yougotthis-noonanews.netlify.app/top-headlines?q=아이유`
+  );
   const response = await fetch(url);
   const data = await response.json(); // json은 파일 형식중 하나
   news = data.articles;
-  console.log(`data: ${news}`);
-  console.log(`hi1`);
+  console.log(news);
 };
 getLatestNews();
+// http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/
