d59dbe6d4954b9b58b64fea17da0d516e68cafc0
Subin <ryusb981211@gmail.com>
2024-07-09 14:50:38 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// const API_KEY = 'a7c947ae7ecb44d6baf7c8b20bcd20e8';
+let news = [];
+const getLatestNews = async () => {
+  // const a = 'hello';
+  const url = new URL(
+    'http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines'
+  );
+  const response = await fetch(url);
+  const data = await response.json(); // json은 파일 형식중 하나
+  news = data.articles;
+  console.log(news);
+};
+getLatestNews();
