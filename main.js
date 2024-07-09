cc97e7a7c1539aa0f8bad4aafd01670df905290b
Subin <ryusb981211@gmail.com>
2024-07-09 15:03:43 +0900

    third commit

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // const API_KEY = 'a7c947ae7ecb44d6baf7c8b20bcd20e8';
 let news = [];
 const getLatestNews = async () => {
-  // const a = 'hello';
-  const url = new URL('https://yougotthis-noonanews.netlify.app/top-headlines');
+  const url = new URL(`https://yougotthis-noonanews.netlify.app/top-headlines`);
   const response = await fetch(url);
   const data = await response.json(); // json은 파일 형식중 하나
   news = data.articles;
-  console.log(news);
+  console.log(`new: ${news}`);
 };
 getLatestNews();
